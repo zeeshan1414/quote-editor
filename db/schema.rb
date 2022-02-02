dbb499e1dbf3e907d989303508208945dcf3dd0e
Zeeshan Hussain <zeeshan1414@gmail.com>
2022-02-02 20:45:23 +0530

    Add simple CRUD controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_02_01_170833) do
+
+  create_table "quotes", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
